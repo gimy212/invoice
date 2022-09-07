86c1903de9ac9a0cd34cd2ad60c74cef0c092442
gimy <mg7934548@gmail.com>
2022-09-07 06:36:19 +0200

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer opened -->
+	<div class="main-footer ht-40">
+		<div class="container-fluid pd-t-0-f ht-100p">
+			<span> © 2022 <a href="#"></a>. Designed by <a href="https://www.facebook.com/profile.php?id=100011418479791">gimy</a></span>
+		</div>
+	</div>
+<!-- Footer closed -->
